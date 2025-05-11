e1eedb7c6936ecf1aed9c735697c158c1e8424fa
Zaman Gabdrakhmanov <z.gabdrakhmanov@vkteam.ru>
2025-05-11 13:52:03 +0300

    add: time measurement and bring out some params for configuration to aco algo

--- tsp_aco.py
+++ tsp_aco.py
@@ -2,6 +2,8 @@ import numpy as np
 import pandas as pd
 import random
 
+import time
+
 # Загрузка матрицы расстояний
 matrix = pd.read_csv('distance_matrix.csv', index_col=0).values
 N = matrix.shape[0]
@@ -14,6 +16,15 @@ BETA = 5.0   # влияние расстояния
 RHO = 0.5    # коэффициент испарения феромона
 Q = 100      # количество феромона, выделяемого муравьем
 
+# Параметр частоты промежуточного вывода
+
+LOG_INTERVAL = 50
+
+max_iteration_duration = 0
+min_iteration_duration = float("inf")
+
+algo_start_time = time.perf_counter()
+
 # Инициализация феромонов
 pheromone = np.ones((N, N))
 
@@ -25,6 +36,8 @@ best_length = float('inf')
 best_route = None
 
 for iteration in range(NUM_ITER):
+    iteration_start_time = time.perf_counter()
+
     all_routes = []
     all_lengths = []
     for ant in range(NUM_ANTS):
@@ -57,10 +70,20 @@ for iteration in range(NUM_ITER):
             a, b = route[i], route[(i+1)%N]
             pheromone[a, b] += Q / length
             pheromone[b, a] += Q / length
-    if iteration % 50 == 0:
+
+    iteration_end_time = time.perf_counter()
+    iteration_duration = iteration_end_time - iteration_start_time
+    max_iteration_duration = max(max_iteration_duration, iteration_duration)
+    min_iteration_duration = min(min_iteration_duration, iteration_duration)
+
+    if iteration % LOG_INTERVAL == 0:
         print(f"Итерация {iteration}: лучшая длина = {best_length}")
+
+algo_end_time = time.perf_counter()        
 
 # Вывод результата
 best_route = [i + 1 for i in best_route]
 print("Лучший маршрут:", best_route)
 print("Длина маршрута:", best_length) 
+print(f"Общая длительность выполнения алгоритма: {(algo_end_time - algo_start_time):.2f} секунд")
+print(f"Длителность выполнения итерации: max: {max_iteration_duration:.6f} секунд, min: {min_iteration_duration:.6f} секунд")
